24f6921c04638b78039f5434343b1ba46806af6d
Mithrandir14 <dramadsk@gmail.com>
2020-05-08 17:56:48 +0000

    Added boot-up sequence and player ID

---
+++ main.py
@@ -0,0 +1,8 @@
+#opening sequence
+print("BootUpSeq")
+print("Initialize")
+print("Dvorak 14.1.8")
+print("Ready")
+print(input("your name?"))
+player_name =  input("your name? >")
+print("Your name is {}".format(player_name))
