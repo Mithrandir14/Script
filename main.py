f87c53ebb12fecb298f2f37bcd31fb56b8da2f71
HandsomeSB <zhuyiyun060209@gmail.com>
2020-05-08 18:41:07 +0000

    Removed redundancy for intro

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 #opening sequence
+def hi():
+  print("test")
+  hi()
 print("BootUpSeq")
 print("Initialize")
 print("Dvorak 14.1.8")
 print("Ready")
 print(input("your name?"))
 player_name =  input("your name? >")
-print("Your name is {}".format(player_name))
 print("Your name is {}. You are an {}.".format(player_name, "insignificant human"))
+
+
+class question:
+  question = ""
+  def __init__(self, something):
+    self.question = something
+
+
+question = question("")
