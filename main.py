e37ca4450dc08e3e14c20a478ee7d66b9e678545
HandsomeSB <zhuyiyun060209@gmail.com>
2020-05-08 18:53:53 +0000

    class test

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ print("Ready")
 print(input("your name?"))
 player_name =  input("your name? >")
 print("Your name is {}. You are an {}.".format(player_name, "insignificant human"))
-
+print("I am Lambda, tasked to remove you from this place. To do this, I need you to solve a series of puzzles.")
 
 class question:
   question = ""
-  def __init__(self, something):
-    self.question = something
+  def __init__(self, question):
+    self.question = question
 
 
-question = question("")
+question = question("test")
+print(question.question)
